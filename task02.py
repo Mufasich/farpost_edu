c8a41e5fdad1ce03f0a3c4796205fe8775308294
Mufasich <mufasich@icloud.com>
2025-03-11 19:31:57 +1000

    Загрузил таску 02

--- task02.py
+++ task02.py
@@ -6,8 +6,21 @@ Given a cell with "it's a fib sequence" from slideshow,
 We guarantee, that the given sequence contain >= 0 integers inside.
 
 """
-from collections import Sequence
+from collections.abc import Sequence
 
 
-def check_fibonacci(data: Sequence[int]) -> bool:
-    ...
+def check_fibonacci(data: Sequence) -> bool:
+    if len(data) < 2: return False
+
+    for i in range(2, len(data)):
+        if data[i] != data[i - 1] + data[i - 2]: return False
+
+    return True
+
+
+
+
+
+
+print(check_fibonacci([0, 1, 1, 2, 3, 5]))
+print(check_fibonacci([0, 1, 1, 2, 4, 5]))
